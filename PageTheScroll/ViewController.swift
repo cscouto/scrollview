1791a98b1df33c5b879f1e6d168aa116163e9312
Tiago Henrique <tiagocouto@Tiagos-iMac.local>
2017-01-11 14:56:38 -0800

    final

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var scroll: UIScrollView!
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0
+        
+        for x in 0...2{
+            let image =  UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scroll.frame.size.width / 2 + scroll.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scroll.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height/2)-75, width: 150, height: 150)
+        }
+        
+        scroll.clipsToBounds = false
+        scroll.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 
